143fae79fb5a296532d7df1e65321c75ab8e6645
sofiberry <sofi2darling@gmail.com>
2024-07-13 23:38:43 +0300

    refactor: skeleton card

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width='400px' borderRadius='10px' overflow='hidden'>
-      <Skeleton height='200px' />
+    <Card>
+      <Skeleton height='280px' />
       <CardBody>
         <SkeletonText />
       </CardBody>
