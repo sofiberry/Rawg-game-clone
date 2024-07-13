83303a51b6236b25234bfd109b73de2c99b1ebbb
sofiberry <sofi2darling@gmail.com>
2024-07-13 21:50:27 +0300

    Displaying Platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,11 +8,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding={12}
-        spacing={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding={3} spacing={10}>
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
