36a61f87de7f31f992e677d0b8497500f78e25d7
sofiberry <sofi2darling@gmail.com>
2024-07-30 10:01:37 +0300

    Toggle the Mode text

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -2,16 +2,35 @@ import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+  const [mode, setMode] = useState("Light");
+  const changeText = () => {
+    toggleColorMode();
+    setMode(mode === "Dark" ? "Light" : "Dark");
+  };
   return (
     <HStack>
       <Switch
-        colorScheme='green'
+        id='mode'
         isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
+        onChange={changeText}
       />
-      <Text whiteSpace='nowrap'>Dark Mode</Text>
+      <Text whiteSpace='nowrap'>
+        <label htmlFor='mode'>{mode} Mode</label>
+      </Text>
     </HStack>
   );
+
+  // const { toggleColorMode, colorMode } = useColorMode();
+  // return (
+  //   <HStack>
+  //     <Switch
+  //       colorScheme='green'
+  //       isChecked={colorMode === "dark"}
+  //       onChange={toggleColorMode}
+  //     />
+  //     <Text whiteSpace='nowrap'>Dark Mode</Text>
+  //   </HStack>
+  // );
 };
 
 export default ColorModeSwitch;
